5c216525ccecd7b11ebd4d227697c9f912a02513
Cristian Buffa <bufface@gmail.com>
2016-07-15 16:29:17 -0300

    Fix en el modelo Usuarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
          :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
-    where(provider: auth[:facebook], uid: auth[:uid]).first_or_create do |user|
+    where(provider: auth[:provider], uid: auth[:uid]).first_or_create do |user|
       if auth[:info]
         user.email = auth[:info][:email]
         user.name = auth[:info][:name]
       end
-      user.password = Devise.friendly_token(0, 20)
+      user.password = Devise.friendly_token[0, 20]
     end
   end
 end
